92fcd4134f80018ca3d0ac15359e9a5b8747fc0a
Renata <rselleshiy@gmail.com>
2020-10-29 17:11:19 -0700

    Add code and comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
 const randomUserUrl = 'https://randomuser.me/api/?results=12';
-const search = document.getElementsByClassName('search-container');
 const gallery = document.getElementById('gallery');
 const card = document.querySelector('.card');
 
-//Using fetch to fetch the random users
+//using fetch to fetch the random users
 //then parsing the data to json
 //finally calling the generateCard function to display data to the page
+//(used "Displaying the Content" video to set up this fetch request)
 fetch(randomUserUrl) 
     .then(res => res.json())
     .then(data => generateCard(data.results))
 
-
+//creating the generateCards function 
+//looping thru the results of the people generated
+//using placeholders to retrieve data with the given results
+//using “Handle Multiple Promises with Promise.all” video
 function generateCard(data) {
-    const getCards = data.map(results => { 
-    `
+    data.map(results => { 
+        const section = document.createElement('section');
+        gallery.appendChild(section);
+        section.innerHTML = `
     <div class="card">
     <div class="card-img-container">
         <img class="card-img" src="${results.picture.large}" alt="profile picture">
@@ -25,32 +30,13 @@ function generateCard(data) {
             </div>
         </div>
     `;
-        });
-        gallery.innerHTML = getCards;
-    }
+    });
+}
 
-   
-    generateCard();
- 
 
-// `
-// <div class="modal-container">
-// <div class="modal">
-//     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-//     <div class="modal-info-container">
-//         <img class="modal-img" src="${results.picture.thumbnail}" alt="profile picture">
-//         <h3 id="name" class="modal-name cap">${results.name.first} ${results.name.last}</h3>
-//         <p class="modal-text">${results.email}</p>
-//         <p class="modal-text cap">${results.location.city}</p>
-//         <hr>
-//         <p class="modal-text">${results.phone}</p>
-//         <p class="modal-text">${results.location.street.number} ${results.location.street.name} 
-//         ${results.location.city}, ${results.location.state}
-//         ${results.location.postcode}</p>
-//         <p class="modal-text">Birthday: ${results.dob.date}</p>
-//     </div>
-// </div>
-// `;
+
+
+
 
 
 

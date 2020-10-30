626c7c3bc9f7ee0fa0758a9b879d86012db3b352
Renata <rselleshiy@gmail.com>
2020-10-30 12:05:13 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -13,27 +13,25 @@ fetch(randomUserUrl)
 //creating the generateCards function 
 //looping thru the results of the people generated
 //using placeholders to retrieve data with the given results
-//using “Handle Multiple Promises with Promise.all” video
+//helpful links: “Handle Multiple Promises with Promise.all”, doggoselect workspace
 function generateCard(data) {
-    data.map(results => { 
-        // const section = document.createElement('section');
-        // gallery.appendChild(section);
-        gallery.innerHTML =
-         `
-    <div class="card">
-    <div class="card-img-container">
-        <img class="card-img" src="${results.picture.large}" alt="profile picture">
+    const getCards = data.map(results => { `
+        <div class="card">
+        <div class="card-img-container">
+            <img class="card-img" src="${results.picture.large}" alt="profile picture">
+                </div>
+                <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${results.name.first} ${results.name.last}</h3>
+                    <p class="card-text">${results.email}</p>
+                    <p class="card-text cap">${results.location.city}, ${results.location.state}</p>
+                </div>
             </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">${results.name.first} ${results.name.last}</h3>
-                <p class="card-text">${results.email}</p>
-                <p class="card-text cap">${results.location.city}, ${results.location.state}</p>
-            </div>
-        </div>
-    `;
-    });
-   
+        `}).join('');
+        gallery.innerHTML = getCards;
 }
+
+
+
 
 
 

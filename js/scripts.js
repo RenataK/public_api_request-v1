70f54d09f0cd648719db8836270c0a515caa1e5f
Renata <rselleshiy@gmail.com>
2020-12-07 17:45:00 -0800

    Update script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,46 @@
-const randomUserUrl = ['https://randomuser.me/api/?results=12'];
+const randomUserUrl = 'https://randomuser.me/api/?nat=us&results=12';
+const searchContainer = document.querySelector('.search-container');
 const gallery = document.getElementById('gallery');
 const body = document.querySelector('body');
 let employeeData = [];
 
-/** using fetch to fetch the random users
-then parsing the data to json
-finally calling the generateCard function to display data to the page
-(used "Displaying the Content" video to set up this fetch request)
+const loadEmployees = async () => {
+    try {
+        const res = await fetch(randomUserUrl) 
+        const data = await res.json();
+            generateCard(data.results);
+            employeeData.push(...data.results);
+            createModalPopout();
+            searchBar();
+    } 
+    catch (err) {
+        console.error(err);
+    }
+}
 
-**/
+function searchBar() {
+    const search = `
+    <form action="#" method="get">
+        <input type="search" id="search-input" class="search-input" placeholder="Search...">
+        <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
+    </form>
+    `;
+    searchContainer.insertAdjacentHTML('beforeend', search);
 
-fetch(randomUserUrl) 
-    .then(res => res.json())
-    .then(data => {
-        generateCard(data.results);
-        employeeData.push(...data.results);
-        console.log(employeeData);
-    })  
-    .then(() => createModalPopout())
+    const searchBar = document.getElementById('search-input');
+    searchBar.addEventListener('keyup', (e) => {
+        const searchString = e.target.value.toLowerCase();
+        //console.log(searchString);
+        const filteredCharacters = employeeData.filter((results) => {
+            return ( 
+                results.name.first.toLowerCase().includes(searchString) ||
+                results.name.last.toLowerCase().includes(searchString) 
+                ) 
+        });
+        console.log(filteredCharacters);
+        generateCard(filteredCharacters);
+      });
+}
 
 /** creating the generateCards function 
 looping thru the results of the people generated
@@ -37,8 +60,7 @@ function generateCard(data) {
                 </div>
             </div>
         `}).join('');
-        gallery.innerHTML = getCards;
-        //gallery.insertAdjacentHTML('beforeend', getCards);
+        gallery.insertAdjacentHTML('beforeend', getCards);
 }
 
 /** looping around the cards and attaching a click listener to each,
@@ -50,10 +72,6 @@ function createModalPopout() {
     for (let i=0; i<card.length; i++) {
         card[i].addEventListener('click', (e) => {
            let index = [...card].indexOf(e.currentTarget);
-            // if (e.composedPath().includes(card[i])) {
-                // console.log(e.composedPath());
-                // console.log(e.target);
-                //generateModal(card[i]);
                 generateModal(employeeData[index]);
         });
     }
@@ -65,19 +83,20 @@ function generateModal(person) {
     <div class="modal">
         <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
         <div class="modal-info-container">
-            <img class="modal-img" src="${person.picture.medium}" alt="profile picture">
+            <img class="modal-img" src="${person.picture.large}" alt="profile picture">
             <h3 id="name" class="modal-name cap">${person.name.first} ${person.name.last}</h3>
             <p class="modal-text">${person.email}</p>
             <p class="modal-text cap">${person.location.city}</p>
             <hr>
-            <p class="modal-text">${person.phone}</p>
+            <p class="modal-text">${person.cell.slice(0, 5)} ${person.cell.slice(6, 14)}</p>
             <p class="modal-text">${person.location.street.number} ${person.location.street.name} 
             ${person.location.city}, ${person.location.state}
             ${person.location.postcode}</p>
-            <p class="modal-text">Birthday: ${person.dob.date}</p>
+            <p class="modal-text">Birthday: ${person.dob.date.slice(5, 7)}/${person.dob.date.slice(8, 10)}/${person.dob.date.slice(2, 4)}</p>
         </div>
     </div>
     `;
+
         document.body.insertAdjacentHTML('beforeend', modalContainer);
 
         const button = document.getElementById('modal-close-btn');
@@ -88,3 +107,4 @@ function generateModal(person) {
     });
 }
 
+loadEmployees();
